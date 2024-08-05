6c27ade920385edb1fbea3a33e52bc8ce38652a6
mayank1365 <mayank1365@gmail.com>
2024-08-05 21:11:37 +0530

    updated path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { dates } from '/home/mayank/codes/dev/dodgyDave/utils/dates.js'
+import { dates } from 'utils/dates.js'
 import OpenAI from 'openai'
 
 const tickersArr = []
@@ -38,7 +38,26 @@ const loadingArea = document.querySelector('.loading-panel')
 const apiMessage = document.getElementById('api-message')
 
 async function fetchStockData() {
-    
+    document.querySelector('.action-panel').style.display = 'none'
+    loadingArea.style.display = 'flex'
+    try {
+        const stockData = await Promise.all(tickersArr.map(async (ticker) => {
+            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${process.env.POLYGON_API_KEY}`
+            const response = await fetch(url)
+            const data = await response.text()
+            const status = await response.status
+            if (status === 200) {
+                apiMessage.innerText = 'Creating report...'
+                return data
+            } else {
+                loadingArea.innerText = 'There was an error fetching stock data.'
+            }
+        }))
+        fetchReport(stockData.join(''))
+    } catch(err) {
+        loadingArea.innerText = 'There was an error fetching stock data.'
+        console.error('error: ', err)
+    }
 }
 
 
