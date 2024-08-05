e85924b21f98658540601bc8c2d7ef7aa90fa34d
mayank1365 <mayank1365@gmail.com>
2024-08-05 21:08:23 +0530

    functionalities added

---
+++ index.js
@@ -0,0 +1,77 @@
+import { dates } from '/home/mayank/codes/dev/dodgyDave/utils/dates.js'
+import OpenAI from 'openai'
+
+const tickersArr = []
+
+const generateReportBtn = document.querySelector('.generate-report-btn')
+
+generateReportBtn.addEventListener('click', fetchStockData)
+
+document.getElementById('ticker-input-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const tickerInput = document.getElementById('ticker-input')
+    if (tickerInput.value.length > 2) {
+        generateReportBtn.disabled = false
+        const newTickerStr = tickerInput.value
+        tickersArr.push(newTickerStr.toUpperCase())
+        tickerInput.value = ''
+        renderTickers()
+    } else {
+        const label = document.getElementsByTagName('label')[0]
+        label.style.color = 'red'
+        label.textContent = 'You must add at least one ticker. A ticker is a 3 letter or more code for a stock. E.g TSLA for Tesla.'
+    }
+})
+
+function renderTickers() {
+    const tickersDiv = document.querySelector('.ticker-choice-display')
+    tickersDiv.innerHTML = ''
+    tickersArr.forEach((ticker) => {
+        const newTickerSpan = document.createElement('span')
+        newTickerSpan.textContent = ticker
+        newTickerSpan.classList.add('ticker')
+        tickersDiv.appendChild(newTickerSpan)
+    })
+}
+
+const loadingArea = document.querySelector('.loading-panel')
+const apiMessage = document.getElementById('api-message')
+
+async function fetchStockData() {
+    
+}
+
+
+async function fetchReport(data) {
+    const messages = [
+        { 
+            role: "system",
+            content: "You are a stock analyst. Please write a report on the stock data provided." 
+        },
+        { 
+            role: "user",
+            content: data
+        }
+    ];
+
+    try {
+        const response = await OpenAI.chat.completions.create({
+            model: 'gpt-4',
+            messages: messages
+        });
+
+        console.log(response);
+    } catch (error) {
+        console.error("Error fetching report:", error);
+    }
+}
+
+
+function renderReport(output) {
+    loadingArea.style.display = 'none'
+    const outputArea = document.querySelector('.output-panel')
+    const report = document.createElement('p')
+    outputArea.appendChild(report)
+    report.textContent = output
+    outputArea.style.display = 'flex'
+}
